c2fc23be1d444ad0a1d1331ee2de73e2fd21e082
mtelek <marcitelek@gmail.com>
2026-04-29 23:52:12 +0200

    fix: database and adapter issue for prisma

--- frontend/src/lib/prisma.ts
+++ frontend/src/lib/prisma.ts
@@ -7,7 +7,7 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
