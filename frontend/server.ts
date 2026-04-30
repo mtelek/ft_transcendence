d647b70208c0d5a204ceb9e57346f2f11cc75b16
mtelek <marcitelek@gmail.com>
2026-04-30 20:36:11 +0200

    feat: running website with https instead of http, add self signed certificate generation

--- frontend/server.ts
+++ frontend/server.ts
@@ -1,8 +1,9 @@
-import { createServer } from "http";
+import { createServer } from "https";
 import { Server } from "socket.io";
 import next from "next";
 import { createPokerServerState } from "./server/poker/state";
 import { registerPokerHandlers } from "./server/poker/handlers";
+import fs from "fs";
 
 export type { PokerCard, GameSnapshot } from "./server/poker/types";
 
@@ -13,12 +14,17 @@ const handle = app.getRequestHandler();
 // APP
 
 app.prepare().then(() => {
-  const httpServer = createServer((req, res) => handle(req, res));
+  const options = {
+    key: fs.readFileSync("/app/certs/key.pem"),
+    cert: fs.readFileSync("/app/certs/cert.pem"),
+  };
+  const httpServer = createServer(options, (req, res) => handle(req, res));
+  //const httpServer = createServer((req, res) => handle(req, res));
   const io = new Server(httpServer, { cors: { origin: "*" } });
   const state = createPokerServerState();
   registerPokerHandlers(io, state);
 
   httpServer.listen(3000, () => {
-    console.log("> Ready on http://localhost:3000");
+    console.log("> Ready on http://0.0.0.0:3000");
   });
 });
